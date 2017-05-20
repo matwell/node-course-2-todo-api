a0300b9f355eb91fd2fa6b09c1871733c24b233f
matwell <marcus.atwell@gmail.com>
2017-05-20 17:07:18 +0930

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {
+  ObjectID
+} = require('mongodb');
 
 var {
   mongoose
@@ -37,11 +40,31 @@ app.get('/todos', (req, res) => {
   });
 });
 
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({
+      todo
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
 
 
 module.exports = {
+  app
+} = {
   app
 }
