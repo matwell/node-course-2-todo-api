a90196b088f29001883708f14e6ef82ea69b0b4b
matwell <marcus.atwell@gmail.com>
2017-05-27 17:37:10 +0930

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -17,6 +17,10 @@ var {
   User
 } = require('./models/user');
 
+var {
+  authenticate
+} = require('./middleware/authenticate');
+
 var app = express();
 const port = process.env.PORT;
 
@@ -124,6 +128,12 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
